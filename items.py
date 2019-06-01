bd3d9022662614821b385239138b883a5efd96d4
Stefan Horst <stefan.horst@grandcentrix.net>
2019-06-01 16:09:06 +0200

    init

---
+++ items.py
@@ -0,0 +1,25 @@
+from pyfiglet import Figlet
+
+f = Figlet(width=120)
+figlet = f.renderText(node.hostname)
+
+files = {}
+
+if node.metadata.get('motd', False):
+    files["/etc/motd"] = {
+        'content': node.metadata['motd'],
+        'content_type': 'jinja2',
+        'owner': "root",
+        'group': "root",
+        'mode': "0644",
+        'context': {'figlet': figlet.split('\n')},
+    }
+else:
+    files["/etc/motd"] = {
+        'source': "motd_template",
+        'content_type': 'jinja2',
+        'owner': "root",
+        'group': "root",
+        'mode': "0644",
+        'context': {'figlet': figlet.split('\n')},
+    }
